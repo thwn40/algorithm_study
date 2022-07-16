9886f9c4f6c73252c165b90e96c67a7e510bed56
gangdodan <dodannnn4@gmail.com>
2022-07-16 16:42:12 +0900

    미로탐색_2178,숨바꼭질_1697

--- JiWon/week1/미로탐색_2178.java
+++ JiWon/week1/미로탐색_2178.java
@@ -1,47 +1,59 @@
 package JiWon.week1;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class 미로탐색_2178 {
-    static int[] visited = new int[10001];
-    static int[] move = {-1, 1, 2};
+    static int[][] board;
+    static int n;
+    static int m;
+    static boolean[][] visited;
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, 1, -1};
 
-    public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        int n = in.nextInt();
-        int k = in.nextInt();
-        System.out.println(BFS(n, k));
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        board = new int[n][m];
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+            for (int j = 0; j < m; j++) {
+                board[i][j] = s.charAt(j) - '0';
+            }
+        }
+        visited = new boolean[n][m];
+        visited[0][0] = true;
+        bfs(0, 0);
+        System.out.println(board[n - 1][m - 1]);
     }
 
-    public static int BFS(int n, int k) {
-        Queue<Integer> q = new LinkedList();
-        visited[n] = 1;
-        q.offer(n);
-        int level = 0;
+    public static void bfs(int x, int y) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[]{x, y});
+
         while (!q.isEmpty()) {
-            int len = q.size();
-            for (int i = 0; i < len; i++) {
-                int x = q.poll();
-                for (int j = 0; j < 3; j++) {
-                    int nx;
-                    if (move[j] == 2) {
-                        nx = x * move[j];
-                    } else {
-                        nx = x + move[j];
-                    }
-                    if (nx == k) {
-                        return level + 1;
-                    }
-                    if (nx > 0 && nx <= 10000 && visited[nx] == 0) {
-                        visited[nx] = 1;
-                        q.offer(nx);
-                    }
-                }
+            int[] now = q.poll();
+            int tx = now[0];    //x 대입
+            int ty = now[1];    //y
+            for (int i = 0; i < 4; i++) {   //현재 위치에 다음 위치 대입
+                int nx = tx + dx[i];
+                int ny = ty + dy[i];
+                if (nx < 0 || ny < 0 || nx >= n || ny >= m)     //보드 범위에서 벗어나면 아무것도 안하고 넘어가세유
+                    continue;
+                if (visited[nx][ny] || board[nx][ny] == 0)  //방문한 적 있거나 보드가 0이면 넘어가삼유
+                    continue;
+                //next actions
+                q.add(new int[]{nx, ny});   //else 현재 위치 좌표 큐에 넣긔
+                board[nx][ny] = board[tx][ty] + 1;  //보드 현재 위치에 이전 위치 + 1 = 카운트++
+                visited[nx][ny] = true;     //방문 체크
             }
-            level++;
         }
-        return 0;
     }
 }
