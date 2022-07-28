984c5de48e5683f327d2b97e2294b0f97aaf8458
hipopotamus <qkqntjddn@gmail.com>
2022-07-28 18:40:25 +0900

    약수의합_17425

---
+++ SeongWoo1/week4/골드바흐의추측_6588.java
@@ -0,0 +1,11 @@
+package SeongWoo1.week4;
+
+public class 골드바흐의추측_6588 {
+
+    public boolean isPrime(int number) {
+        return false;
+    }
+    public static void main(String[] args) {
+
+    }
+}
--- SeongWoo1/week4/약수의합_17425.java
+++ SeongWoo1/week4/약수의합_17425.java
@@ -1,8 +1,32 @@
 package SeongWoo1.week4;
 
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class 약수의합_17425 {
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        long[] sumDivisors = new long[1000000 + 1];
+        long[] cumulative = new long[1000000 + 1];
+
+        for (int i = 1; i <= 1000000; i++) {
+            for (int j = i; j <= (1000000 / i); j++) {
+                if (i == j) {
+                    sumDivisors[i * j] += i;
+                } else {
+                    sumDivisors[i * j] += i + j;
+                }
+            }
+        }
+
+        for (int i = 1; i <= 1000000; i++) {
+            cumulative[i] = cumulative[i - 1] + sumDivisors[i];
+        }
+
+        for (int i = 0; i < n; i++) {
+            int number = scanner.nextInt();
+            System.out.println(cumulative[number]);
+        }
     }
 }
