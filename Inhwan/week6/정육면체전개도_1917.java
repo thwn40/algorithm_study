33aa3f07153217250a9b2df5bdaf3d3424a9b584
Cutaku <mmzz0219@gmail.com>
2022-08-15 08:04:09 +0900

    큐빙_5373, 톱니바퀴_15662, 로봇청소기_14503, 정육면체전개도_1917

---
+++ Inhwan/week6/정육면체전개도_1917.java
@@ -0,0 +1,93 @@
+package Inhwan.week6;
+
+import java.util.*;
+import java.io.*;
+
+public class 정육면체전개도_1917 {
+    static int[][] Planar;
+    static boolean[][] check = new boolean[6][];
+    static boolean temp;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        for (int t = 0; t < 3; t++) {
+            boolean isCube = true;
+
+            Planar = new int[6][];
+            for (int i = 0; i < 6; i++) {
+                Planar[i] = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            }
+
+            for (int i = 0; i < 6; i++) {
+                for (int j = 0; j < 6; j++) {
+
+                    if (Planar[i][j]==1) {
+                        temp = false;
+
+                        for (int k = 0; k < 6; k++) {
+                            check[k] = new boolean[6];
+                        }
+
+                        Planar[i][j] = 2;
+                        check[i][j] = true;
+
+                        List<Integer> X = new ArrayList<>(), Y=new ArrayList<>();
+                        X.add(0);
+                        Y.add(0);
+                        dfs(i,j,X ,Y,0);
+
+                        isCube = isCube && temp;
+                    }
+                }
+            }
+
+            if (isCube) System.out.println("yes");
+            else System.out.println("no");
+        }
+    }
+
+    static void dfs(int i, int j, List<Integer> X, List<Integer> Y, int d) {
+        int x = X.stream().mapToInt(Integer::intValue).sum();
+        int y = Y.stream().mapToInt(Integer::intValue).sum();
+
+        if (Planar[i+x][j+y]==1 && test(X,Y,d)) {
+            Planar[i+x][j+y]=2;
+            temp=true;
+            return;
+        }
+
+        int[][] D = {{1,0},{0,1},{-1,0},{0,-1}};
+
+        for (int[] dir : D) {
+            if (temp) return;
+
+            x+=dir[0];
+            y+=dir[1];
+
+            if (i+x>=0 && j+y>=0 && i+x<6 && j+y<6 && !check[i+x][j+y] && Planar[i+x][j+y]>0) {
+                check[i+x][j+y]=true;
+                X.add(dir[0]);
+                Y.add(dir[1]);
+                dfs(i,j,X,Y,d+1);
+                check[i+x][j+y]=false;
+                X.remove(d+1);
+                Y.remove(d+1);
+            }
+            x-=dir[0];
+            y-=dir[1];
+        }
+    }
+
+    static boolean test(List<Integer> X, List<Integer> Y, int d) {
+        int x = X.stream().mapToInt(Integer::intValue).sum();
+        int y = Y.stream().mapToInt(Integer::intValue).sum();
+
+        if (d==2 && x*y==0) return true;
+        if (d==3 && x*x*y*y==4 && X.get(1)==X.get(3) && Y.get(1)==Y.get(3)) return true;
+        if (d==4 && x*x*y*y==16 && X.get(1)==X.get(4) && Y.get(1)==Y.get(4)) return true;
+        if (d==5 && x*x*y*y==36 && (X.get(1)+X.get(5))*(X.get(2)+X.get(3)+X.get(4))+(Y.get(1)+Y.get(5))*(Y.get(2)+Y.get(3)+Y.get(4))==0) return true;
+
+        return false;
+    }
+}
---
+++ Inhwan/week6/톱니바퀴2_15662.java
@@ -0,0 +1,81 @@
+package Inhwan.week6;
+
+import java.util.*;
+import java.io.*;
+
+
+public class 톱니바퀴2_15662 {
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+
+        Cogwheel[] cogwheels = new Cogwheel[T];
+
+        for (int i = 0; i < T; i++) {
+            cogwheels[i] = new Cogwheel(Arrays.stream(br.readLine().split("")).mapToInt(Integer::parseInt).toArray());
+        }
+
+        int K = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < K; i++) {
+            int[] rotation = Arrays.stream(br.readLine().split(" ")).mapToInt(Integer::parseInt).toArray();
+            int start = rotation[0]-1;
+            int d = rotation[1];
+
+            cogwheels[start].rotate(d);
+
+            for (int j = start + 1; j<T; j++) {
+                if (cogwheels[j-1].teeth[2+d]==cogwheels[j].teeth[6]) d*=0;
+                d*=-1;
+
+                cogwheels[j].rotate(d);
+            }
+
+            d = rotation[1];
+
+            for (int j = start - 1; j>=0; j--) {
+                if (cogwheels[j].teeth[2]==cogwheels[j+1].teeth[6+d]) d*=0;
+                d*=-1;
+
+                cogwheels[j].rotate(d);
+            }
+        }
+
+        int sum=0;
+
+        for (int i = 0; i < T; i++) {
+            sum+=cogwheels[i].teeth[0];
+        }
+
+        System.out.println(sum);
+    }
+
+    static class Cogwheel {
+        int[] teeth;
+
+        Cogwheel(int[] teeth) {
+            this.teeth = teeth;
+        }
+
+        void rotate(int d) {
+            int temp;
+
+            if (d==1) {
+                temp = teeth[7];
+                for (int i = 7; i > 0; i--) {
+                    teeth[i]=teeth[i-1];
+                }
+                teeth[0]=temp;
+            }
+
+            if (d==-1) {
+                temp = teeth[0];
+                for (int i = 0; i < 7; i++) {
+                    teeth[i]=teeth[i+1];
+                }
+                teeth[7]=temp;
+            }
+        }
+    }
+}
