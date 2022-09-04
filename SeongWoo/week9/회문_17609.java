a8b28430a6c6893959e627e1e73b5d6a62f425b6
hipopotamus <qkqntjddn@gmail.com>
2022-09-04 10:39:01 +0900

    week9

--- SeongWoo/week9/회문_17609.java
+++ SeongWoo/week9/회문_17609.java
@@ -5,12 +5,15 @@ import java.util.Scanner;
 public class 회문_17609 {
 
     //문자열 str이 회문인지 검사하는 메서드
-    private static int checkPalindrome(String str, int start, int end) {
+    private static int checkPalindrome(String str, int start, int end, int depth) {
+        if (depth >= 2) {
+            return 0;
+        }
+
         int count = 0;
-        //start가 가르키는 문자를 지웠을 때 결과
-        int deleteStartResult = 2;
-        //end가 가르키는 문자를 지웠을 때 결과
-        int deleteEndResult = 2;
+        //start, end가 가르키는 문자를 지웠을 때 결과값
+        int deleteStartResult = 1;
+        int deleteEndResult = 1;
         //문자를 하나 지웠는지를 판단하는 flag
         boolean deleteFlag = false;
 
@@ -22,20 +25,22 @@ public class 회문_17609 {
             }
             //start가 가르키는 문자를 지웠을 때 회문이 될 수 있는 경우
             //start의 문자를 지움 => start + 1 부터 검사
-            //start + 1부터 end까지의 문자열이 회문인지 다시 검사해서 그 결과값을 받는다.
+            //start + 1부터 end까지의 문자열이 회문인지 다시 검사해서 회문이 되지 않을 경우 1을 반환 받는다.
             if (start + 1 <= end && str.charAt(start + 1) == str.charAt(end)) {
                 deleteFlag = true;
-                deleteStartResult = checkPalindrome(str, start + 1, end);
+                deleteStartResult = checkPalindrome(str, start + 1, end, depth + 1);
             }
+            //end가 가르키는 문자를 지웠을 때 회문이 될 수 있는 경우. 이하 동일
             if (end - 1 >= start && str.charAt(start) == str.charAt(end - 1)) {
                 deleteFlag = true;
-                deleteEndResult = checkPalindrome(str, start, end - 1);
+                deleteEndResult = checkPalindrome(str, start, end - 1, depth + 1);
             }
 
             //문자를 삭제 했다면 count를 늘려주고, start와 end를 지운 경우의 결과값에서 가장 최소가되는 결과값(최선의 경우)을 count에 더해준다.
             if (deleteFlag) {
                 count++;
                 count += Math.min(deleteStartResult, deleteEndResult);
+            //유사회문도 될 수 없는 경우
             } else {
                 count = 2;
             }
@@ -43,12 +48,7 @@ public class 회문_17609 {
         }
 
         //count의 결과에 따라 return값 반환
-        if (count >= 2) {
-            return 2;
-        } else if (count == 1) {
-            return 1;
-        }
-        return 0;
+        return count;
     }
 
     public static void main(String[] args) {
@@ -57,7 +57,7 @@ public class 회문_17609 {
 
         for (int i = 0; i < caseSize; i++) {
             String str = scanner.next();
-            System.out.println(checkPalindrome(str, 0, str.length() - 1));
+            System.out.println(checkPalindrome(str, 0, str.length() - 1 ,0));
         }
     }
 }
