002188c33a7f73dad86b56ceb1814a78db521d3e
박성우 <77265289+hipopotamus@users.noreply.github.com>
2023-06-02 11:30:22 +0900

    week41 (#90)
    LCS, N-Queen, 평범한 배낭, 플로이드

---
+++ SeongWoo/week41/LCS.java
@@ -0,0 +1,38 @@
+package SeongWoo.week41;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class LCS {
+
+    public static int dp(int[][] memorization, String firstString, String secondString) {
+
+        for (int i = 1; i < memorization.length; i++) {
+            for (int j = 1; j < memorization[0].length; j++) {
+                if (firstString.charAt(i - 1) == secondString.charAt(j - 1)) {
+                    memorization[i][j] = memorization[i - 1][j - 1] + 1;
+                } else {
+                    memorization[i][j] = Math.max(memorization[i - 1][j], memorization[i][j - 1]);
+                }
+            }
+        }
+
+        return memorization[memorization.length - 1][memorization[0].length - 1];
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        String firstString = st.nextToken();
+
+        st = new StringTokenizer(br.readLine());
+        String secondString = st.nextToken();
+
+        int[][] memorization = new int[firstString.length() + 1][secondString.length() + 1];
+
+        int result = dp(memorization, firstString, secondString);
+        System.out.println(result);
+    }
+}
